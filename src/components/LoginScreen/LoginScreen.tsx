d5ec6ced813f3de48562f27e486f73144dd4538c
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2025-09-14 10:34:03 -0300

    02-fixed branch conflicts

--- src/components/LoginScreen/LoginScreen.tsx
+++ src/components/LoginScreen/LoginScreen.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 import Logo from "../img/MediBook-Logo.png";
-import "../style/Login.css";
+import "./Login.css";
 
 
 
